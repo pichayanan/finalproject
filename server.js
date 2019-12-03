bac0d298719e785aa51f9fdd1f5dc3de326e7d4d
pichayanan <38246387+pichayanan@users.noreply.github.com>
2019-12-03 17:43:40 +0700

    Ans4

--- server.js
+++ server.js
@@ -40,6 +40,35 @@ router.get('/products', products.getAllProducts);
 router.get('/products/:pid', products.getProductById);
 
 // #4 Complete the routing for POST, PUT, DELETE
+app.post('/api/products',function (req, res) {
+    var newproduct =req.body;
+    var product = new Product(newproduct);
+    product.save(function (err){
+        if(err) res.status(500).json(err);
+        res.json({status: "Added a product"});
+    });
+    
+});
+
+
+app.put('/api/products/:id',function (req, res) {
+    var id = req.params.id;
+    var updateproduct = req.body;
+    Product.findByIdAndUpdate(id, updateproduct, function(err){
+        if (err) res.status(500).json(err);
+        res.json({status: "updated a product"});
+    })
+   
+});
+app.delete('/api/products/:id',function (req, res) {
+    var id = req.params.id;
+    var updateproduct = req.body;
+    Product.findByIdAndUpdate(id, updateproduct, function(err){
+        if (err) res.status(500).json(err);
+        res.json({status: "delete a product"});
+    })
+   
+});
 
 // ===============================
 
